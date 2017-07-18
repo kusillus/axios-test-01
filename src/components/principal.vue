675753ecc688f6c7f0ed3d41f7c89a244c9c63b2
Alexander Guevara Benites <wguevarab7@gmail.com>
2017-07-17 23:26:08 -0500

    Se realizaron cambios en la vista, se agrego algunos parametros para la consulta a la API, se agrego etiqueta meta viewport para el responsive

--- src/components/principal.vue
+++ src/components/principal.vue
@@ -1,25 +1,30 @@
 <template lang="html">
   <div class="pt-4 container">
     <img class="principal-img" src="../assets/camera.svg">
-    <div class="">
+    <div class="header-principal py-3">
       <h3>¿Selecciona tu opcion a buscar?</h3>
       <span>probando axios</span>
     </div>
-    <div class="col d-flex justify-content-around">
-      <div v-for="item in checkboxs">
-          <label class="checkboxs-container" :for="item.value" v-on:click="clear()">{{item.name}}</label>
-          <input class="d-none" type="radio" :id="item.value" :value="item.value" v-model="picked">
+    <div class="col d-flex justify-content-around flex-wrap">
+      <div class="col-11 col-sm-3  my-1" v-for="item in checkboxs">
+        <input class="d-none" type="radio" :id="item.value" :value="item.value" v-model="picked">
+        <label class="checkboxs-container py-2 col m-0" :for="item.value" v-on:click="clear()">{{item.name}}</label>
       </div>
     </div>
-    <span>Picked: {{ picked.length }}</span>
-    <span>Pokenum: {{ pokeNum.length }}</span>
-    <div class="container box-dex mt-4 py-2 px-5 mb-2">
-      <h5 class="py-3">Ingresa un numero</h5>
-      <input class="form-control" type="text" name="" placeholder="24" v-model="pokeNum">
-      <button class="btn" type="button" name="button" v-on:click="submitpk()">Consultar</button>
-      <div class="pt-2">
-        <span class="poke-sub">Tu pokemon es:</span>
-        <p class="poke-res">{{pokeRes}}</p>
+    <div class="container box-dex mt-1 px-5 mb-2 d-flex flex-wrap justify-content-center">
+      <h5 class="py-2 col-12 m-0">Ingresa un numero</h5>
+      <input class="form-control col-10" type="text" name="" placeholder="24" v-model="pokeNum">
+      <button class="btn col-6 btn-submit my-2" type="button" name="button" v-on:click="submitpk()">Consultar</button>
+      <div class="mt-2 col-12 result-query-container">
+        <span class="poke-sub">{{message}}</span>
+        <div class="d-flex result-query justify-content-center align-items-center">
+          <img class="px-1" :src="pokeImg" alt="">
+          <p class="poke-res m-0">{{pokeRes}}</p>
+        </div>
+        <div  v-if="query" class="info-query col-12 d-flex flex-wrap justify-content-center my-3">
+          <p class="q-title col-12 py-2">Tu consulta es:</p>
+          <p class="q-query col-11">https://pokeapi.co/api/v2/"+<span class="poke">{{picked}}</span>+"/" +<span class="poke">{{pokeNum}}</span></p>
+        </div>
       </div>
       <div v-if="emptyNum" class="">
         Debes de ingresar un numero o nombre
@@ -38,11 +43,15 @@ import cCheckbox from './checkboxs.vue'
 export default {
   data () {
     return {
+      query: false,
       emptyNum: false,
       emptyPic: false,
       pokeNum: '',
       pokeRes: '',
+      pokeImg: '',
       picked: '',
+      message: '',
+      move: 'http://www.pokexperto.net/nds/itemdex/imagenes/329.gif',
       checkboxs: [
         {
           name: "Pokemon",
@@ -92,8 +101,20 @@ export default {
       vm.pokeRes = 'buscando...'
       axios.get("https://pokeapi.co/api/v2/"+ vm.picked +"/" + vm.pokeNum)
             .then(function(response) {
+              vm.query = true
+              vm.message = 'tu '+ vm.picked+' es:'
               let res = response.data
               vm.pokeRes = res.name
+              if (vm.picked == 'pokemon') {
+                vm.pokeImg = res.sprites.front_default
+                console.log('pack')
+              }
+              if(vm.picked == 'item'){
+                vm.pokeImg = res.sprites.default
+              }
+              if (vm.picked == 'move') {
+                vm.pokeImg = vm.move
+              }
             })
             .catch(function(error) {
               vm.pokeRes = 'Nel'
